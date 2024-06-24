09140359544239e3489085437192136756eab0b0
shafayetejaman <shafayetejaman@gmail.com>
2024-06-24 14:10:36 +0000

    Mon, Jun 24, 2024, 8:10 PM +06:00

--- backend/online_teacher/account/models.py
+++ backend/online_teacher/account/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import User
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="account")
+    date_available = models.CharField()
