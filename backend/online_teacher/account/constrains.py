aa401606c5dbee7d3c10f1e7827bfbf8a09cba49
shafayetejaman <shafayetejaman@gmail.com>
2024-06-28 08:45:40 +0000

    Fri, Jun 28, 2024, 2:45 PM +06:00

--- backend/online_teacher/account/constrains.py
+++ backend/online_teacher/account/constrains.py
@@ -1,10 +1,18 @@
-dayes=(
-    ("Sa","Sa"),
-    ("su","su"),
-    ("sm","sa"),
-    ("sa","sa"),
-    ("sa","sa"),
-    ("sa","sa"),
-    ("sa","sa"),
-    ("sa","sa"),
-)
+
+SUNDAY = 1
+MONDAY = 2
+TUESDAY = 3
+WEDNESDAY = 4
+THURSDAY = 5
+FRIDAY = 6
+SATURDAY = 7
+
+DAY_CHOICES = (
+      (SUNDAY, 'Sunday'),
+      (MONDAY, 'Monday'),
+      (TUESDAY, 'Tuesday'),
+      (WEDNESDAY, 'Wednesday'),
+      (THURSDAY, 'Thursday'),
+      (FRIDAY, 'Friday'),
+      (SATURDAY, 'Saturday'),
+  )
--- backend/online_teacher/account/models.py
+++ backend/online_teacher/account/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from constrains import dayes
+from constrains import DAY_CHOICES
+
 # Create your models here.
 
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="account")
-    week_days_option = models.CharField(choices=dayes)
+    week_days_option = models.CharField(choices=DAY_CHOICES)
