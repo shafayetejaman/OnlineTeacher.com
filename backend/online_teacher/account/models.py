d1adf08f3728e7c1b49ca6a0cecb2182c9ee8a01
shafayetejaman <shafayetejaman@gmail.com>
2024-06-28 06:46:28 +0000

    Fri, Jun 28, 2024, 12:46 PM +06:00

--- backend/online_teacher/account/models.py
+++ backend/online_teacher/account/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from constrains import 
+from constrains import dayes
 # Create your models here.
 
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="account")
-    date_available = models.CharField(choices=)
+    week_days_option = models.CharField(choices=dayes)
