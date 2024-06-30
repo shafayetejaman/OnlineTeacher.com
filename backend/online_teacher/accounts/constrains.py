109a2fd5cb705833b21ead7d006e5342db0463df
shafayetejaman <shafayetejaman@gmail.com>
2024-06-30 10:23:44 +0000

    Sun, Jun 30, 2024, 4:23 PM +06:00

---
+++ backend/online_teacher/accounts/constrains.py
@@ -0,0 +1,40 @@
+SUNDAY = 1
+MONDAY = 2
+TUESDAY = 3
+WEDNESDAY = 4
+THURSDAY = 5
+FRIDAY = 6
+SATURDAY = 7
+
+DAY_CHOICES = (
+    (SUNDAY, "Sunday"),
+    (MONDAY, "Monday"),
+    (TUESDAY, "Tuesday"),
+    (WEDNESDAY, "Wednesday"),
+    (THURSDAY, "Thursday"),
+    (FRIDAY, "Friday"),
+    (SATURDAY, "Saturday"),
+)
+
+SSC = 10
+HSC = 12
+CSE = 13
+EEE = 14
+BA = 15
+MA = 16
+GRADE9 = 9
+GRADE11 = 11
+GRADE8 = 8
+
+
+QUALIFICATION = (
+    (SSC, "SSC"),
+    (HSC, "HSC"),
+    (CSE, "B.Sc in CSE"),
+    (EEE, "B.Sc in EEE"),
+    (BA, "BA"),
+    (MA, "MA"),
+    (GRADE9, "Class 9"),
+    (GRADE11, "Class 11"),
+    (GRADE8, "Class 8"),
+)
